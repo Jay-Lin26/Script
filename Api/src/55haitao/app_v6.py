960915b8f1405e166a2d85648924c710225a8166
jay-lin26 <z64666760@163.com>
2021-12-01 17:17:18 +0800

    接口自动化

--- Api/src/55haitao/app_v6.py
+++ Api/src/55haitao/app_v6.py
@@ -2,6 +2,7 @@ import xlrd
 from xlutils.copy import copy
 import requests
 from Api.utils.dingtalk import DingTalk
+import time
 
 
 class ApiAutoTest:
@@ -13,41 +14,43 @@ class ApiAutoTest:
         self.write_sheet = self.write_book.get_sheet(0)
         self.host = "https://appv6.55haitao.com"
         self.sendMsg = DingTalk()
-
-    def writeWork(self, row=0, col=0, content=""):
-        self.write_sheet.write(2, 7, content)
-        self.write_book.save("new_api.xlsx")
+        self.nowTime = time.strftime("%Y-%m-%d %H:%M")
 
     def readWork(self):
         rows = self.main_sheet.nrows
-        for i in range(1, rows):
-            method = self.main_sheet.cell(i, 1).value
-            url = self.host + self.main_sheet.cell(i, 2).value
-            headers = eval(self.main_sheet.cell(i, 3).value)  # 字符串转字典需要用eval函数
-            params = self.main_sheet.cell(i, 4).value
-            msg = self.main_sheet.cell(i, 5).value
-            if method == 'get':
-                data = requests.get(url=url, headers=headers, params=params).json()
-                if data["msg"] == msg:
-                    self.write_sheet.write(i, 6, "2021-12-01")
-                    self.write_sheet.write(i, 7, "pass")
-                if data["msg"] != msg:
-                    self.write_sheet.write(i, 6, "2021-12-01")
-                    self.write_sheet.write(i, 7, "fail")
-                    self.write_sheet.write(i, 8, data["msg"])
-                    ding = "接口 {} 返回结果异常 '{}'; 请及时排查！{}"
-                    self.sendMsg.execution("55Haitao", ding.format(url, data["msg"], "2021-12-01"))
-            elif method == 'post':
-                data = requests.post(url=url, headers=headers, json=params).json()
-                if data["msg"] == msg:
-                    self.write_sheet.write(i, 6, "2021-12-01")
-                    self.write_sheet.write(i, 7, "pass")
-                if data["msg"] != msg:
-                    self.write_sheet.write(i, 6, "2021-12-01")
-                    self.write_sheet.write(i, 7, "fail")
-                    self.write_sheet.write(i, 8, data["msg"])
-                    ding = "接口 {} 返回结果异常 '{}'; 请及时排查！{}"
-                    self.sendMsg.execution("55Haitao", ding.format(url, data["msg"], "2021-12-01"))
+        # 不同设备所使用的header参数'p'有所不同;
+        platform = ["web", "wap", "iOS", "BaiduApplet"]
+        for p in platform:
+            for i in range(1, rows):
+                method = self.main_sheet.cell(i, 1).value
+                url = self.host + self.main_sheet.cell(i, 2).value
+                headers = eval(self.main_sheet.cell(i, 3).value)  # 字符串转字典需要用eval函数
+                headers["token"] = "020d787a7e7ccac7756c19e46f9ee626"
+                headers["p"] = p
+                params = self.main_sheet.cell(i, 4).value
+                msg = self.main_sheet.cell(i, 5).value
+                if method == 'get':
+                    data = requests.get(url=url, headers=headers, params=params).json()
+                    if data["msg"] == msg:
+                        self.write_sheet.write(i, 6, self.nowTime)
+                        self.write_sheet.write(i, 7, "pass")
+                    if data["msg"] != msg:
+                        self.write_sheet.write(i, 6, self.nowTime)
+                        self.write_sheet.write(i, 7, "fail")
+                        self.write_sheet.write(i, 8, data["msg"])
+                        ding = '接口 {} 返回结果异常" platform = {}; msg = {} "; 请及时排查！{}'
+                        self.sendMsg.execution("55Haitao", ding.format(url, p, data["msg"], self.nowTime))
+                elif method == 'post':
+                    data = requests.post(url=url, headers=headers, json=params).json()
+                    if data["msg"] == msg:
+                        self.write_sheet.write(i, 6, self.nowTime)
+                        self.write_sheet.write(i, 7, "pass")
+                    if data["msg"] != msg:
+                        self.write_sheet.write(i, 6, self.nowTime)
+                        self.write_sheet.write(i, 7, "fail")
+                        self.write_sheet.write(i, 8, data["msg"])
+                        ding = '接口 {} 返回结果异常" platform = {}; msg = {} "; 请及时排查！{}'
+                        self.sendMsg.execution("55Haitao", ding.format(url, p, data["msg"], self.nowTime))
         self.write_book.save("new_api.xlsx")
 
 
