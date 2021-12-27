b68efb2889097bb131e03c6dc7c28dbb677e2b6d
jay-lin26 <z64666760@163.com>
2021-12-27 16:50:10 +0800

    优化，新增接口

--- Api/src/haitao/app_v6.py
+++ Api/src/haitao/app_v6.py
@@ -10,11 +10,11 @@ from xlutils.copy import copy
 current_path = os.getcwd()
 sys.path.append("C:\\Users\\55haitao\\Desktop\\Study\\Api")
 
-from src.utils.dingtalk import DingTalk
+from utils.dingtalk import DingTalk
 
 
 class ApiAutoTest:
-    def __init__(self, sheet_name="haitao"):
+    def __init__(self, sheet_name="55haitao", environments="pro"):
         file_path = "C:\\Users\\55haitao\\Desktop\\api.xlsx"
         try:
             book = xlrd.open_workbook(file_path)
@@ -22,8 +22,27 @@ class ApiAutoTest:
             self.allSheet = book.sheet_names()
             self.main_sheet = book.sheet_by_name(sheet_name)
             self.write_sheet = self.write_book.get_sheet(0)
-            self.host = "https://appv6.55haitao.com"
-            # self.host = "https://app.test.55haitao.com"
+            if sheet_name == "55haitao":
+                if environments == "pro":
+                    self.host = "https://appv6.55haitao.com"
+                elif environments == "dev":
+                    self.host = "https://app.test.55haitao.com"
+                else:
+                    print("请输入正确的环境名称！！！")
+                    sys.exit()
+            elif sheet_name == "maxrebates":
+                if environments == "pro":
+                    self.host = "https://api.maxrebates.com"
+                elif environments == "dev":
+                    self.host = "https://api.staging.51huaji.com"
+                else:
+                    print("请输入正确的环境名称！！！")
+                    sys.exit()
+            else:
+                print("请输入正确的网站名称！！！")
+                sys.exit()
+
+            self.website = sheet_name
             self.sendMsg = DingTalk()
         except FileNotFoundError:
             print("%s is not found" % file_path)
@@ -31,7 +50,7 @@ class ApiAutoTest:
 
     def readWork(self):
         rows = self.main_sheet.nrows
-        # 不同设备所使用的header参数'p'有所不同;
+        # 不同设备所使用的header参数'p'不同;
         platform = ["web", "wap", "iOS", "BaiduApplet"]
         for i in range(1, rows):
             now_time = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -42,6 +61,7 @@ class ApiAutoTest:
             headers = eval(self.main_sheet.cell(i, 3).value)  # 字符串转字典需要用eval函数;
             # headers["token"] = "c032a1b5ff24292124f5e4b292e9a9c9"  # 测试
             headers["token"] = "020d787a7e7ccac7756c19e46f9ee626"   # 正式
+
             try:
                 params = eval(self.main_sheet.cell(i, 4).value)
                 if features == "showAdd":
@@ -53,8 +73,13 @@ class ApiAutoTest:
                     text = now_time + " 自动发布测试；请勿审核"
                     params["content"] = text
                     platform = ["iOS"]
+                elif features == "loseorderAdd":
+                    text = now_time + "测试"
+                    params["order_number"] = text
+                    platform = ["iOS", "web", "android"]
             except SyntaxError:
                 params = " "
+
             for p in platform:
                 headers["p"] = p
                 msg = self.main_sheet.cell(i, 5).value
@@ -63,28 +88,30 @@ class ApiAutoTest:
                     if data["msg"] == msg:
                         self.write_sheet.write(i, 6, now_time)
                         self.write_sheet.write(i, 7, "pass")
-                    if data["msg"] != msg:
+                    else:
                         self.write_sheet.write(i, 6, now_time)
                         self.write_sheet.write(i, 7, "fail")
                         self.write_sheet.write(i, 8, data["msg"])
                         ding = ' 接口 {} 返回结果异常 "platform : {}; msg : {}"; {}'
-                        self.sendMsg.execution("55Haitao", ding.format(path, p, data["msg"], now_time))
+                        self.sendMsg.execution(self.website, ding.format(path, p, data["msg"], now_time))
                         break   # 跳出本次设备型号循环，开始执行下一个接口
                 elif method == 'post':
                     data = requests.post(url=url, headers=headers, json=params).json()
                     if data["msg"] == msg:
                         self.write_sheet.write(i, 6, now_time)
                         self.write_sheet.write(i, 7, "pass")
-                    if data["msg"] != msg:
+                    else:
                         self.write_sheet.write(i, 6, now_time)
                         self.write_sheet.write(i, 7, "fail")
                         self.write_sheet.write(i, 8, data["msg"])
                         ding = ' 接口 {} 返回结果异常 "platform : {}; msg : {}"; {}'
-                        self.sendMsg.execution("55Haitao", ding.format(path, p, data["msg"], now_time))
+                        self.sendMsg.execution(self.website, ding.format(path, p, data["msg"], now_time))
                         break
-        self.write_book.save("apiReport.xlsx")
+        self.write_book.save("C:\\Users\\55haitao\\Desktop\\apiReport.xlsx")
 
 
 if __name__ == '__main__':
-    a = ApiAutoTest()
+    a = ApiAutoTest(sheet_name="55haitao", environments="pro")
     a.readWork()
+    b = ApiAutoTest(sheet_name="maxrebates", environments="pro")
+    b.readWork()
--- Api/utils/common.py
+++ Api/utils/common.py
@@ -3,7 +3,3 @@ import time
 
 def changeTime():
     a = time.strftime("%Y-%m-%d %H:%M")
-
-
-if __name__ == '__main__':
-    changeTime()
