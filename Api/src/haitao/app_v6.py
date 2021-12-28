122d33da653b14c6eb49abe2e0985674bf027b00
jay-lin26 <z64666760@163.com>
2021-12-28 15:54:52 +0800

    优化，新增接口

--- Api/src/haitao/app_v6.py
+++ Api/src/haitao/app_v6.py
@@ -43,6 +43,7 @@ class ApiAutoTest:
                 sys.exit()
 
             self.website = sheet_name
+            self.env = environments
             self.sendMsg = DingTalk()
         except FileNotFoundError:
             print("%s is not found" % file_path)
@@ -53,32 +54,48 @@ class ApiAutoTest:
         # 不同设备所使用的header参数'p'不同;
         platform = ["web", "wap", "iOS", "BaiduApplet"]
         for i in range(1, rows):
-            now_time = time.strftime("%Y-%m-%d %H:%M:%S")
+            now_time = time.strftime("%Y-%m-%d-%H-%M-%S")
             method = self.main_sheet.cell(i, 1).value
             path = self.main_sheet.cell(i, 2).value
             url = self.host + self.main_sheet.cell(i, 2).value
             features = self.main_sheet.cell(i, 9).value  # 是否自动修改参数
             headers = eval(self.main_sheet.cell(i, 3).value)  # 字符串转字典需要用eval函数;
-            # headers["token"] = "c032a1b5ff24292124f5e4b292e9a9c9"  # 测试
-            headers["token"] = "020d787a7e7ccac7756c19e46f9ee626"   # 正式
+            if self.env == "pro":
+                headers["token"] = "020d787a7e7ccac7756c19e46f9ee626"   # 正式
+            elif self.env == "dev":
+                headers["token"] = "c032a1b5ff24292124f5e4b292e9a9c9"  # 测试
+            else:
+                return "Token错误"
 
             try:
                 params = eval(self.main_sheet.cell(i, 4).value)
                 if features == "showAdd":
-                    text = now_time + " 自动发布测试"
+                    text = now_time + " 晒单测试；请勿审核"
+                    images = [
+                        {"image": "https://cdn.55haitao.com/bbs/data/attachment/forum/202112/21/202112211047435024.jpg",
+                         "is_cover": "1",
+                         "width": 1200,
+                         "height": 1200,
+                         "tags": []
+                         }
+                    ]
+                    if self.env == "dev":
+                        images[0]["image"] = "https://cdn-test.55haitao.com/bbs/data/attachment/deal/2021/12/28/6800c8cff496ea0016145623968ddd92d2d.png"
                     params["title"] = text
-                    params["images"] = json.dumps(params["images"])
+                    params["images"] = json.dumps(images)
                     platform = ["iOS"]
                 elif features == "commentAdd":
-                    text = now_time + " 自动发布测试；请勿审核"
+                    text = now_time + " 评论测试；请勿审核"
                     params["content"] = text
                     platform = ["iOS"]
                 elif features == "loseorderAdd":
-                    text = now_time + "测试"
+                    text = "TextOrder_" + str(round(time.time()))
                     params["order_number"] = text
                     platform = ["iOS", "web", "android"]
             except SyntaxError:
                 params = " "
+            except TypeError:
+                return "接口参数不规范或格式错误"
 
             for p in platform:
                 headers["p"] = p
@@ -113,5 +130,3 @@ class ApiAutoTest:
 if __name__ == '__main__':
     a = ApiAutoTest(sheet_name="55haitao", environments="pro")
     a.readWork()
-    b = ApiAutoTest(sheet_name="maxrebates", environments="pro")
-    b.readWork()
--- Libs/project/demo.py
+++ Libs/project/demo.py
@@ -1,14 +1,5 @@
-import requests
-import json
-a = '{"category":2,"content":"自动发布请勿审核","images":[{"image":"https://55haitao-test.oss-cn-qingdao.aliyuncs.com/bbs/data/attachment/forum/202112/06/image_of1xK_202112061741","is_cover":"1","width":1200,"height":1200,"tags":[]}],"question_id":0,"tags":"70078","title":"16:44自动发布测试"}'
-b = eval(a)
-image = b["images"]
-b["images"] = json.dumps(image)
-print(b["images"])
-url = "https://appv6.55haitao.com/show/add"
-headers = {
-    "v": "v3.1",
-    "token": "020d787a7e7ccac7756c19e46f9ee626"
-}
-data = requests.post(url=url, headers=headers, json=b).json()
-print(data)
+import time
+text = "TextOrder_" + time.strftime("%Y-%m-%d-%H-%M-%S")
+text1 = time.time()
+print(text)
+print(str(round(text1)))
